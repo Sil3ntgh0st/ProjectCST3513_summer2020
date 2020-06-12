01ad7591492c3f3b689824ead6d478912629df6c
HelloImFranky <47162800+HelloImFranky@users.noreply.github.com>
2020-06-12 19:50:31 -0400

    Game Start/Home page
    Both pages work with one another as of today, next challenge would be to make 3 new windows for the 3 buttons =] Good luck everyone!

--- VillageGUI/src/villageWindow/views/villageGUIhome.java
+++ VillageGUI/src/villageWindow/views/villageGUIhome.java
@@ -6,9 +6,23 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JButton;
+import javax.swing.JSeparator;
+import javax.swing.ImageIcon;
 
 public class villageGUIhome extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
 	private JPanel contentPane;
 
 	/**
@@ -32,11 +46,116 @@ public class villageGUIhome extends JFrame {
 	 */
 	public villageGUIhome() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 400, 467);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		
+		JLabel lblNewLabel_2 = new JLabel(" Click on Jobs to collect items!");
+		
+		JLabel lblNewLabel_3 = new JLabel(" Click on Inventory check items!\n");
+		
+		JLabel lblNewLabel = new JLabel("Welcome Villager!");
+		lblNewLabel.setFont(new Font("Luminari", Font.PLAIN, 13));
+		
+		JLabel lblNewLabel_1 = new JLabel("A villager must work");
+		lblNewLabel_1.setVerticalAlignment(SwingConstants.TOP);
+		lblNewLabel_1.setFont(new Font("Luminari", Font.PLAIN, 13));
+		
+		JLabel lblNewLabel_4 = new JLabel(" Click on Crafting to make items!");
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addGap(90)
+							.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblNewLabel_2)
+								.addComponent(lblNewLabel_3, GroupLayout.PREFERRED_SIZE, 217, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblNewLabel_4)))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addGap(129)
+							.addComponent(lblNewLabel_1))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addGap(137)
+							.addComponent(lblNewLabel)))
+					.addContainerGap(83, Short.MAX_VALUE))
+		);
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 32, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(lblNewLabel_1)
+					.addGap(18)
+					.addComponent(lblNewLabel_2)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(lblNewLabel_3)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(lblNewLabel_4, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(7, Short.MAX_VALUE))
+		);
+		panel.setLayout(gl_panel);
+		
+		JPanel panel_1 = new JPanel();
+		
+		JButton btnCrafting = new JButton("Crafting");
+		btnCrafting.setIcon(new ImageIcon(villageGUIhome.class.getResource("/villageWindow/source/workbench.png")));
+		
+		JButton btnInventory = new JButton("Inventory");
+		btnInventory.setIcon(new ImageIcon(villageGUIhome.class.getResource("/villageWindow/source/backpack.png")));
+		
+		JButton btnJobsButton = new JButton("   Jobs");
+		btnJobsButton.setIcon(new ImageIcon(villageGUIhome.class.getResource("/villageWindow/source/miner.png")));
+		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
+		gl_panel_1.setHorizontalGroup(
+			gl_panel_1.createParallelGroup(Alignment.TRAILING)
+				.addGap(0, 400, Short.MAX_VALUE)
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addContainerGap(30, Short.MAX_VALUE)
+					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
+						.addComponent(btnJobsButton, GroupLayout.PREFERRED_SIZE, 344, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnCrafting, GroupLayout.PREFERRED_SIZE, 344, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnInventory, GroupLayout.PREFERRED_SIZE, 344, GroupLayout.PREFERRED_SIZE))
+					.addGap(26))
+		);
+		gl_panel_1.setVerticalGroup(
+			gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 264, Short.MAX_VALUE)
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addComponent(btnJobsButton, GroupLayout.PREFERRED_SIZE, 80, GroupLayout.PREFERRED_SIZE)
+					.addGap(12)
+					.addComponent(btnInventory, GroupLayout.PREFERRED_SIZE, 80, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btnCrafting, GroupLayout.PREFERRED_SIZE, 80, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
+		panel_1.setLayout(gl_panel_1);
+		
+		JSeparator separator = new JSeparator();
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(separator, GroupLayout.PREFERRED_SIZE, 390, GroupLayout.PREFERRED_SIZE)
+						.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 390, GroupLayout.PREFERRED_SIZE)
+						.addComponent(panel, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 396, Short.MAX_VALUE))
+					.addContainerGap())
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(separator, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
+					.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+		);
+		contentPane.setLayout(gl_contentPane);
 	}
 
 }
